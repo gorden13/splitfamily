fc3de2765e01ac195f1d5140af05a75b0f4809ac
Горынцев Денис <dl.goryntsev@samolet.ru>
2025-01-17 00:42:07 +0300

    change output folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      app: `${path.resolve(__dirname, "./src/app/")}`,
-      entities: `${path.resolve(__dirname, "./src/entities/")}`,
-      features: path.resolve(__dirname, "./src/features/"),
-      widgets: `${path.resolve(__dirname, "./src/widgets/")}`,
-      pages: `${path.resolve(__dirname, "./src/pages")}`,
-      shared: `${path.resolve(__dirname, "./src/shared")}`,
+      app: `${path.resolve(__dirname, './src/app/')}`,
+      entities: `${path.resolve(__dirname, './src/entities/')}`,
+      features: path.resolve(__dirname, './src/features/'),
+      widgets: `${path.resolve(__dirname, './src/widgets/')}`,
+      pages: `${path.resolve(__dirname, './src/pages')}`,
+      shared: `${path.resolve(__dirname, './src/shared')}`,
     },
   },
   server: {
     watch: {
       usePolling: true,
     },
+  },
+  build: {
+    assetsDir: './docs/assets',
   },
 });
